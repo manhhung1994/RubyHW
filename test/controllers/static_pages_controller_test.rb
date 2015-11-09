365888106d73f47e075235714ffd0a7bebf6c69a
Hung <manhhungvoi@gmail.com>
2015-11-09 22:11:15 +0700

    finish user update, edit, delete

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,13 +14,8 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get about" do
-  	get :about
-  	assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-  test "should get contact" do
-    get :contact
+    get :about
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
- test "invalid signup information" do
+
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
@@ -11,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  test "valid signup information" do
+      test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name:  "Example User",
@@ -20,5 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
